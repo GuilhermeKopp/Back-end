f4f105b5887eb48711dc842cd3bd69d86d1f2665
GuilhermeKopp <guikopp19@gmail.com>
2021-11-10 15:31:27 -0300

    Exceções personalizadas

--- src/main/java/com/guilhermekopp/todo/services/TodoService.java
+++ src/main/java/com/guilhermekopp/todo/services/TodoService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.guilhermekopp.todo.domain.Todo;
 import com.guilhermekopp.todo.repositories.TodoRepository;
+import com.guilhermekopp.todo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TodoService {
@@ -17,7 +18,8 @@ public class TodoService {
 	
 	public Todo findById(Integer id) {
 		Optional<Todo> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Todo.class.getName()));
 	}
 
 	public List<Todo> findAllOpen() {
