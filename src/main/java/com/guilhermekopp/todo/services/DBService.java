67ac0880b5bb3c123c36aa27bffa590932f765fd
GuilhermeKopp <guikopp19@gmail.com>
2021-11-11 15:03:55 -0300

    Ajustes back-end

--- src/main/java/com/guilhermekopp/todo/services/DBService.java
+++ src/main/java/com/guilhermekopp/todo/services/DBService.java
@@ -1,7 +1,7 @@
 package com.guilhermekopp.todo.services;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +16,13 @@ public class DBService {
 	@Autowired
 	private TodoRepository todoRepository;
 	
-	public void instanciaBaseDeDados() {
-	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+	public void instanciaBaseDeDados() throws ParseException {
+	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Todo t1 = new Todo (null, "Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("03/12/2022 10:25", formatter), false);
-		Todo t2 = new Todo (null, "Ler", "Ler livro de desenvolvimento", LocalDateTime.parse("10/11/2021 08:25", formatter), true);
-		Todo t3 = new Todo (null, "Exercicios", "Praticar exercicios", LocalDateTime.parse("10/12/2021 15:15", formatter), false);
-		Todo t4 = new Todo (null, "Programar", "Meditar durante 30 minutos pela manhã", LocalDateTime.parse("09/11/2021 19:30", formatter), true);
+		Todo t1 = new Todo (null, "Estudar", "Estudar Spring Boot 2 e Angular 11", sdf.parse("03/12/2022"), false);
+		Todo t2 = new Todo (null, "Ler", "Ler livro de desenvolvimento", sdf.parse("10/11/2021"), true);
+		Todo t3 = new Todo (null, "Exercicios", "Praticar exercicios", sdf.parse("10/12/2021"), false);
+		Todo t4 = new Todo (null, "Programar", "Meditar durante 30 minutos pela manhã", sdf.parse("09/11/2021"), true);
 		
 		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 	}
